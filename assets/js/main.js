1347bdcb351976ab5f2b9857928c3fd2303eca0a
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-22 20:27:50 +0200

    js added

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+let inputMail 
+
+
+function subscribe(){
+    inputMail = document.getElementById('email').value
+    document.getElementById('thankYou').innerHTML = 'Thank you for your Subscription' + ' 👁 🫦 👁'  
+    console.log(inputMail);
+}
+
